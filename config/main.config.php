26b8a990e88b8d4b2e7e6a410d1dfadf81e3e3b6
Bhaswanth Blan <bhaswanth.u@gmail.com>
2014-09-28 04:35:27 +0530

    Basic skeleton with home_page and site_down static html pages.

--- config/main.config.php
+++ config/main.config.php
@@ -1,6 +1,16 @@
 <?php
 
+	// Site settings go here.
+
+
+
 	// Show errors (Parse errors are not shown by this)
 	// Set to false in production environment.
 	// true | false
 	$_ERRORS_DEBUG = true;
+
+	// Site status up or down. if down a site down page is displayed.
+	// Anything other than 'up' will display the ROOT/static/site_down.html page.
+	$_SITE_STATUS = 'up';
+
+
