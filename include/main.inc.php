b01f3bca46202b7903ecb8fa5aeda9d0d74edf59
Bhaswanth Blan <bhaswanth.u@gmail.com>
2014-09-28 03:16:23 +0530

    error show setting added to main include and main config

--- include/main.inc.php
+++ include/main.inc.php
@@ -1,3 +1,9 @@
 <?php
 
-	require_once (ROOT . DS . 'config' . DS . 'main.inc.php');
+	require_once (ROOT . DS . 'config' . DS . 'main.config.php');
+
+	if($_ERRORS_DEBUG == true)
+	{
+		error_reporting(E_ALL);
+    	ini_set('display_errors','On');
+    }
